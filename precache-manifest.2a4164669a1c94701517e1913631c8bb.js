312ca92539fd873fba4e03f2a5655019f2167988
Rajeshwar Reddy <rajeshwarreddy.kadari@gmail.com>
2019-08-27 23:45:48 +0530

    Updates

---
+++ precache-manifest.2a4164669a1c94701517e1913631c8bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d875d63b9eae9f4176cbd335e002f308",
+    "url": "/react-journal-app/index.html"
+  },
+  {
+    "revision": "936a837ba4afea56edf5",
+    "url": "/react-journal-app/static/css/main.b7cc8429.chunk.css"
+  },
+  {
+    "revision": "7c2af82687c7b2369e8e",
+    "url": "/react-journal-app/static/js/2.72db6d19.chunk.js"
+  },
+  {
+    "revision": "936a837ba4afea56edf5",
+    "url": "/react-journal-app/static/js/main.3c7e04e3.chunk.js"
+  },
+  {
+    "revision": "b946b2c9f277ce103449",
+    "url": "/react-journal-app/static/js/runtime~main.643da0d3.js"
+  }
+]);
